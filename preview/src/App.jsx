80723852493b7464404e73f767c26aeb0e4d709a
aryanbatras <batraaryan03@gmail.com>
2025-12-30 14:14:18 +0530

    Setting up before docs/ repo
    Setting up before docs/ repo

--- preview/src/App.jsx
+++ preview/src/App.jsx
@@ -1,10 +1,8 @@
 import { Button } from "signal-layers";
-import { useState } from "react";
 
 export default function App() {
   return (
     <div className="items-center justify-center flex h-screen w-screen gap-2 p-8">
-        {/* <Switch label="Hello"/> */}
         <Button ripple>Hello World</Button>
     </div>
   );    
